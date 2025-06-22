0c2130abb413f735ce538bfe72d6e729654c5066
yamamoto-tgz <yamamoto.tar.gz@gmail.com>
2025-06-22 15:50:41 +0900

    [Add] nhk

--- newsbot/app.py
+++ newsbot/app.py
@@ -3,15 +3,18 @@ from flask import Flask, render_template
 
 from newsbot.gigazine import Gigazine
 from newsbot.hatena import Hatena
+from newsbot.nhk import Nhk
 
 gigazine = Gigazine()
 hatena = Hatena()
+nhk = Nhk()
 
 
 # Cron job
 def job():
     gigazine.download()
     hatena.download()
+    nhk.download()
 
 
 scheduler = BackgroundScheduler()
@@ -27,8 +30,9 @@ app = Flask(__name__)
 def index():
     gigazine_items = gigazine.items()
     hatena_items = hatena.items()
+    nhk_items = nhk.items()
 
-    all_items = gigazine_items + hatena_items
+    all_items = gigazine_items + hatena_items + nhk_items
     all_items.sort(key=lambda item: item.datetime, reverse=True)
 
     return render_template(
@@ -36,4 +40,5 @@ def index():
         items=all_items,
         gigazine_len=len(gigazine_items),
         hatena_len=len(hatena_items),
+        nhk_len=len(nhk_items),
     )
---
+++ newsbot/nhk.py
@@ -0,0 +1,30 @@
+from datetime import datetime as dt
+
+from newsbot.item import Item
+from newsbot.rss import Rss
+
+
+class Nhk(Rss):
+    def __init__(self):
+        url = "https://www.nhk.or.jp/rss/news/cat0.xml"
+        path = "./rss/nhk.xml"
+        super().__init__(url, path)
+
+    def items(self):
+        items = []
+
+        try:
+            for item in self.read()["rss"]["channel"]["item"]:
+                title = item["title"]
+                link = item["link"]
+                print(item["pubDate"])
+                datetime = dt.strptime(item["pubDate"], "%a, %d %b %Y %H:%M:%S %z")
+                subjects = []
+                source = "nhk"
+
+                item = Item(title, link, datetime, subjects, source)
+
+                items.append(item)
+
+        finally:
+            return items
