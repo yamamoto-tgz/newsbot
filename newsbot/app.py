043ef6ba05f9fbee2e027399174ee2a89a08d7b6
yamamoto-tgz <yamamoto.tar.gz@gmail.com>
2025-06-23 21:14:29 +0900

    [Add] codezine

--- newsbot/app.py
+++ newsbot/app.py
@@ -2,13 +2,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from flask import Flask, render_template
 
+from newsbot.codezine import Codezine
 from newsbot.gigazine import Gigazine
 from newsbot.hatena import Hatena
 from newsbot.nhk import Nhk
 
 load_dotenv(dotenv_path="newsbot.env")
 
-rss_list = [Gigazine(), Hatena(), Nhk()]
+rss_list = [Gigazine(), Codezine(), Hatena(), Nhk()]
 
 
 # Cron job
---
+++ newsbot/codezine.py
@@ -0,0 +1,31 @@
+from datetime import datetime as dt
+
+from newsbot.item import Item
+from newsbot.rss import Rss
+
+
+class Codezine(Rss):
+    def __init__(self):
+        name = "codezine"
+        url = "https://codezine.jp/rss/new/20/index.xml"
+        path = "./rss/codezine.xml"
+        super().__init__(name, url, path)
+
+    def items(self):
+        items = []
+
+        try:
+            for item in self.read()["rss"]["channel"]["item"]:
+                title = item["title"]
+                link = item["link"]
+                print(item["pubDate"])
+                datetime = dt.strptime(item["pubDate"], "%a, %d %b %Y %H:%M:%S %z")
+                subjects = []
+                source = self.name
+
+                item = Item(title, link, datetime, subjects, source)
+
+                items.append(item)
+
+        finally:
+            return items
