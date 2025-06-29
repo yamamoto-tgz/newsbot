28e8e47573858a364a9f4ea9e016e9282b2bdc2d
yamamoto-tgz <yamamoto.tar.gz@gmail.com>
2025-06-29 09:39:25 +0900

    [Clean] sqlalchemyを導入

--- newsbot/app.py
+++ newsbot/app.py
@@ -2,12 +2,33 @@ from datetime import datetime, timedelta, timezone
 
 import dateutil.parser as dateparser
 import feedparser
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, jsonify, render_template, request
+from sqlalchemy import create_engine, desc, inspect
+from sqlalchemy.dialects.sqlite import insert
+from sqlalchemy.orm import sessionmaker
 
-from newsbot.models import Article
+from newsbot.models import Article, Base
 
 # Database
-Article.create_table()
+engine = create_engine("sqlite:///newsbot.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+
+# Cron
+def job():
+    with Session() as session:
+        threshold_date = datetime.now(timezone.utc) - timedelta(days=30)
+        rowcount = session.query(Article).filter(Article.created <= threshold_date).delete()
+        session.commit()
+        print(f"deleted: {rowcount}")
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(job, "cron", hour=9, minute=38)
+scheduler.start()
+
 
 # Web app
 app = Flask(__name__)
@@ -15,51 +36,54 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    articles = Article.find_all_order_by_read_asc_published_desc()
+    with Session() as session:
+        articles = session.query(Article).order_by(Article.read).order_by(desc(Article.published))
 
-    counter = {}
-    for article in articles:
-        channel = article.channel
-        counter[channel] = counter.get(channel, 0) + 1
+        counter = {}
+        for article in articles:
+            channel = article.channel
+            counter[channel] = counter.get(channel, 0) + 1
 
-    return render_template("index.html", articles=articles, counter=counter)
+        return render_template("index.html", articles=articles, counter=counter)
 
 
 @app.route("/xml/<channel>", methods=["POST"])
 def post_xml(channel):
     articles = []
+    threshold_date = datetime.now(timezone.utc) - timedelta(days=30)
 
     for entry in feedparser.parse(request.data).entries:
         article = Article(
             channel=channel,
             title=entry.title,
             link=entry.link,
-            published=dateparser.parse(entry.updated),
+            published=dateparser.parse(entry.updated).astimezone(timezone.utc),
             created=datetime.now(tz=timezone.utc),
             read=False,
         )
 
-        threshold = datetime.now(timezone.utc) - timedelta(days=31)
-
-        if article.is_newer_than(threshold):
+        if article.published >= threshold_date:
             articles.append(article)
 
-    inserted_rows = Article.bulk_insert(articles)
-    print(f"{channel}: {inserted_rows} rows are inserted")
+    articles_table = inspect(Article).mapper.local_table
+    values = [a.dict() for a in articles]
+    stmt = insert(articles_table).values(values).on_conflict_do_nothing()
 
-    deleted_rows = Article.delete_older_than((datetime.now(timezone.utc) - timedelta(days=31)))
-    print(f"{channel} {deleted_rows} rows are deleted")
+    with engine.begin() as conn:
+        result = conn.execute(stmt)
+        print(f"{channel}: {result.rowcount} rows added")
 
     return "OK", 201
 
 
 @app.route("/articles/<int:id>", methods=["POST"])
 def update(id):
-    article = Article.find_by_id(id)
+    with Session() as session:
+        article = session.query(Article).filter(Article.id == id).first()
 
-    for k, v in request.get_json().items():
-        setattr(article, k, v)
+        for k, v in request.get_json().items():
+            setattr(article, k, v)
 
-    article.save()
+        session.commit()
 
-    return jsonify(article.__dict__), 200
+        return jsonify(article.dict()), 200
--- newsbot/models.py
+++ newsbot/models.py
@@ -1,20 +1,20 @@
-import sqlite3
+from sqlalchemy import Boolean, Column, DateTime, Integer, Text, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 from datetime import datetime, timezone
 
-import dateutil.parser as dateparser
 
-DATABASE = "newsbot.db"
-
-
-class Article:
-    def __init__(self, id=None, channel=None, title=None, link=None, published=None, created=None, read=None):
-        self.id: int = id
-        self.channel: str = channel
-        self.title: str = title
-        self.link: str = link
-        self.published: datetime = published
-        self.created: datetime = created
-        self.read: bool = read
+class Article(Base):
+    __tablename__ = "articles"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    channel = Column(Text)
+    title = Column(Text)
+    link = Column(Text)
+    published = Column(DateTime)
+    created = Column(DateTime)
+    read = Column(Boolean)
+    __table_args__ = (UniqueConstraint("channel", "title", "link"),)
 
     def time(self):
         diff = datetime.now(timezone.utc) - self.published.replace(tzinfo=timezone.utc)
@@ -32,80 +32,5 @@ class Article:
             days = int(seconds // 86400)
             return f"{days}d"
 
-    def is_newer_than(self, threshold):
-        return self.published > threshold
-
-    def save(self):
-        sql = """
-            UPDATE articles
-            SET channel = ?, title = ?, link = ?, published = ?, created = ?, read = ?
-            WHERE id = ?
-        """
-        values = (
-            self.channel,
-            self.title,
-            self.link,
-            self.published,
-            self.created,
-            self.read,
-            self.id,
-        )
-
-        with sqlite3.connect(DATABASE) as con:
-            con.execute(sql, values)
-
-    @staticmethod
-    def from_row(row):
-        return Article(*row)
-
-    @staticmethod
-    def create_table():
-        sql = """
-            CREATE TABLE IF NOT EXISTS articles (
-                id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                channel TEXT, 
-                title TEXT, 
-                link TEXT, 
-                published TIMESTAMP, 
-                created TIMESTAMP, 
-                read BOOLEAN, 
-                UNIQUE (channel, title, link)
-            )
-        """
-        with sqlite3.connect(DATABASE) as con:
-            cursor = con.cursor()
-            cursor.execute(sql)
-
-    @staticmethod
-    def bulk_insert(articles):
-        sql = """
-            INSERT INTO articles (channel, title, link, published, created, read) 
-                VALUES (?, ?, ?, datetime(?), datetime(?), ?) 
-                ON CONFLICT DO NOTHING
-        """
-        values = [[v for k, v in article.__dict__.items() if k != "id"] for article in articles]
-
-        with sqlite3.connect(DATABASE) as con:
-            rowcount = con.cursor().executemany(sql, values).rowcount
-            return rowcount
-
-    @staticmethod
-    def delete_older_than(dt: datetime):
-        with sqlite3.connect(DATABASE) as con:
-            sql = f"DELETE FROM articles WHERE created < datetime(?)"
-            rowcount = con.cursor().execute(sql, [dt]).rowcount
-            return rowcount
-
-    @staticmethod
-    def find_by_id(id):
-        with sqlite3.connect(DATABASE, detect_types=sqlite3.PARSE_DECLTYPES) as con:
-            sql = "SELECT * FROM articles WHERE id = ?"
-            row = con.cursor().execute(sql, [id]).fetchone()
-            return Article.from_row(row)
-
-    @staticmethod
-    def find_all_order_by_read_asc_published_desc():
-        with sqlite3.connect(DATABASE, detect_types=sqlite3.PARSE_DECLTYPES) as con:
-            sql = "SELECT * FROM articles ORDER BY read ASC, published DESC"
-            rows = con.cursor().execute(sql).fetchall()
-            return [Article.from_row(row) for row in rows]
+    def dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
